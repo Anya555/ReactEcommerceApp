10f80daf389c8ad1d0195fecdb6fdea8053c06e0
Anna Panas <annapanas0906@gmail.com>
2020-06-28 18:05:35 -0400

    proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,10 +3,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target:
-        process.env.NODE_ENV === "production"
-          ? "https://mighty-spire-39962.herokuapp.com"
-          : "http://localhost:3001",
+      target: process.env.PORT || 3001,
       changeOrigin: true,
     })
   );
