db67e543d8ddc5bbd16c5dad86967207fefd89b1
Anna Panas <annapanas0906@gmail.com>
2020-06-30 20:16:09 -0400

    heroku

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target:
-        "https://mighty-spire-39962.herokuapp.com" || "http://localhost:3001",
-      changeOrigin: true,
-    })
-  );
-};
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+// module.exports = function (app) {
+//   app.use(
+//     "/api",
+//     createProxyMiddleware({
+//       target:
+//         "https://mighty-spire-39962.herokuapp.com" || "http://localhost:3001",
+//       changeOrigin: true,
+//     })
+//   );
+// };
