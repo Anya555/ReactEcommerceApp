ec55b58b33b365c756de50fe3c5b3700484c2c33
Anna Panas <annapanas0906@gmail.com>
2020-07-16 18:52:31 -0400

    heroku

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,19 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const environment = process.env;
-const isDeployed =
-  environment.NODE_ENV === "production" ||
-  environment.NODE_ENV === "development";
 module.exports = function (app) {
-  if (isDeployed) {
-    app.use(
-      "/api/*",
-      createProxyMiddleware({
-        target: "http://localhost:3001",
-        secure: false,
-        logLevel: "debug",
-        changeOrigin: true,
-        cookieDomainRewrite: "",
-      })
-    );
-  }
+  app.use(
+    "/api/*",
+    createProxyMiddleware({
+      target: "http://localhost:3001",
+      secure: false,
+      logLevel: "debug",
+      changeOrigin: true,
+      cookieDomainRewrite: "",
+    })
+  );
 };
