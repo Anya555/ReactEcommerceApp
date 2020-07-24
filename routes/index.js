a0a39dc83c7dcc72e85eae7c64d9018cb28817ae
Anna Panas <annapanas0906@gmail.com>
2020-07-23 20:39:44 -0400

    heroku

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
-  //changed to public from build
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
