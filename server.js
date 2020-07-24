879f73a2762b105de687429633b2bc98c5ecf14a
Anna Panas <annapanas0906@gmail.com>
2020-07-23 20:50:55 -0400

    heroku

--- server.js
+++ server.js
@@ -40,9 +40,12 @@ app.use(async (req, res, next) => {
 });
 
 app.use(routes);
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, function () {
