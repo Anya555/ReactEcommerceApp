732c9867e03666da3f275f9c7cd94c1320a34576
Gary T Almes <garytalmes@gmail.com>
2020-07-29 12:24:12 -0400

    Added client/build to repo

---
+++ client/build/precache-manifest.a175a5285ec6a77b0f048779e50516f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09510138962214c6c4d2732edb99a1d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8f1bd9999229a8ff88c",
+    "url": "/static/css/main.11a66319.chunk.css"
+  },
+  {
+    "revision": "804fda3ab36873d1366f",
+    "url": "/static/js/2.bfd9bfa5.chunk.js"
+  },
+  {
+    "revision": "cd7af6418d23afb95d3b8ba5115a90eb",
+    "url": "/static/js/2.bfd9bfa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f1bd9999229a8ff88c",
+    "url": "/static/js/main.2e0f0867.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
